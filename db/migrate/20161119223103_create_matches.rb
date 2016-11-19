1ca42d34484bbf20f7405968f9821ea2e2e9ac36
Juan Edi <jedi@manas.com.ar>
2016-11-19 19:39:17 -0300

    Migrations for users and matches

---
+++ db/migrate/20161119223103_create_matches.rb
@@ -0,0 +1,13 @@
+class CreateMatches < ActiveRecord::Migration[5.0]
+  def change
+    create_table :matches do |t|
+      t.integer :user1_id
+      t.integer :user1_goals
+
+      t.integer :user2_id
+      t.integer :user2_goals
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161119221306) do
+ActiveRecord::Schema.define(version: 20161119223103) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,9 +19,24 @@ ActiveRecord::Schema.define(version: 20161119221306) do
     t.string "name"
   end
 
+  create_table "matches", force: :cascade do |t|
+    t.integer  "user1_id"
+    t.integer  "user1_goals"
+    t.integer  "user2_id"
+    t.integer  "user2_goals"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
   create_table "teams", force: :cascade do |t|
     t.string  "name"
     t.integer "league_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
